a1ddf43ba7d245b77cb781ecc37b95507645d09e
vivhayles <vivhayles@gmail.com>
2024-08-08 12:50:30 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
 function sayHello () {
-const name= prompt ("what is your name?")
-alert ("Hello name " +name + " welcome to my website! ");
+const name= prompt ("What is your name?")
+alert ("Hello " + name + " welcome to my website! ");
 }
 
 sayHello ();
 
-function rollDice (){
-let dice= Math.floor(Math.random()*6) + 1  
-alert ("You have rolled a" + dice + "!")
-}
-
-rollDice ();
 
